4819b780c1318760d29f77b848339d7582cf8189
leng <maryelaine.malit@gmail.com>
2016-11-10 16:37:44 -0500

    new branch with vrae files

---
+++ Variational-Recurrent-Autoencoder/script.py
@@ -0,0 +1,44 @@
+import os
+import numpy as np
+from VRAE import VRAE
+
+from midi import utils
+
+if __name__ == "__main__":
+    music_dir = os.listdir("music")
+    min_length = 71
+    data = np.empty([71,88,69])
+    
+    counter = 0
+    for music in music_dir:
+        music_path = "music/" + music
+        midiread = utils.midiread(music_path)
+        if midiread.piano_roll.shape[0] > min_length:
+            min_index = max(0, (midiread.piano_roll.shape[0]/2) - (min_length/2) - 1)
+            max_index = min((midiread.piano_roll.shape[0]/2) + (min_length/2), midiread.piano_roll.shape[0])
+            piano_roll = midiread.piano_roll[min_index:max_index,:] 
+        data[:,:,counter] = piano_roll
+        counter += 1
+       
+    print(data.shape)
+    print("loaded music")
+    
+    hu_encoder = 400
+    hu_decoder = 400
+    features = 5
+    latent_variables = 20
+    b1 = 0.95
+    b2 = 0.999
+    batch_size = 100
+    n_latent = 20
+    n_epochs = 40
+    learning_rate = 0.001
+    lam = 0
+    
+    my_vrae = VRAE(hu_encoder, hu_decoder, features, \
+     latent_variables, b1, b2, learning_rate, 0.5, batch_size)
+     
+    print("initialized model")
+    
+    my_vrae.create_gradientfunctions(data)
+    print("created gradientfunctions")
