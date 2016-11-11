063b562318e176df19843af90eb6e67e93feaa2c
leng <maryelaine.malit@gmail.com>
2016-11-11 10:34:40 -0500

    script file that works

--- Variational-Recurrent-Autoencoder/script.py
+++ Variational-Recurrent-Autoencoder/script.py
@@ -19,26 +19,57 @@ if __name__ == "__main__":
             piano_roll = midiread.piano_roll[min_index:max_index,:] 
         data[:,:,counter] = piano_roll
         counter += 1
-       
+
     print(data.shape)
     print("loaded music")
-    
+
     hu_encoder = 400
     hu_decoder = 400
-    features = 5
+    features = 88
     latent_variables = 20
     b1 = 0.95
     b2 = 0.999
-    batch_size = 100
+    batch_size = 5
+    #batch_size = 100
     n_latent = 20
-    n_epochs = 40
+    #n_epochs = 40
+    n_epochs = 2
     learning_rate = 0.001
     lam = 0
-    
+    path = "./"
+
     my_vrae = VRAE(hu_encoder, hu_decoder, features, \
      latent_variables, b1, b2, learning_rate, 0.5, batch_size)
-     
+
     print("initialized model")
-    
+
     my_vrae.create_gradientfunctions(data)
     print("created gradientfunctions")
+
+    batch_order = np.arange(int(69 / my_vrae.batch_size))
+    epoch = 0
+    LB_list = []
+
+    print "iterating"
+    while epoch < n_epochs:
+        epoch += 1
+        np.random.shuffle(batch_order)
+        LB = 0.
+
+        for batch in batch_order:
+            batch_LB = my_vrae.updatefunction(batch, epoch)
+            LB += batch_LB
+
+        LB /= len(batch_order)
+
+        LB_list = np.append(LB_list, LB)
+        print "Epoch", epoch, "finished."
+        np.save(path + "LB_list.npy", LB_list)
+        my_vrae.save_parameters(path)
+            
+    print("sampling")
+    my_z = np.random.normal(0, 1, (latent_variables,1))
+    t_steps = 20
+    sample_roll = my_vrae.decode(t_steps, latent_variables, my_z)
+    outfile = "sample.mid"
+    utils.midiwrite(outfile, sample_roll)
